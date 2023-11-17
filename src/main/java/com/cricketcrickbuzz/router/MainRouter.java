93a72a0ee84cd43c01932c54c99511f2a38fe32b
mabhisheksingh <abhishekit13006@gmail.com>
2023-11-17 14:25:21 +0530

    Redis config added

--- src/main/java/com/cricketcrickbuzz/router/MainRouter.java
+++ src/main/java/com/cricketcrickbuzz/router/MainRouter.java
@@ -1,7 +1,10 @@
 package com.cricketcrickbuzz.router;
 
+import com.cricketcrickbuzz.dto.Coffee;
+import com.cricketcrickbuzz.handler.CoffeeHandler;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.MediaType;
 import org.springframework.web.reactive.function.server.RouterFunction;
 import org.springframework.web.reactive.function.server.RouterFunctions;
 import org.springframework.web.reactive.function.server.ServerResponse;
@@ -9,16 +12,19 @@ import org.springframework.web.reactive.function.server.ServerResponse;
 @Configuration
 public class MainRouter {
 
-    private CricketAPI cricketAPI;
+    private final CricketAPI cricketAPI;
+    private final CoffeeHandler coffeeHandler;
 
-    public MainRouter(CricketAPI cricketAPI) {
+    public MainRouter(CricketAPI cricketAPI, CoffeeHandler coffeeHandler) {
         this.cricketAPI = cricketAPI;
+        this.coffeeHandler = coffeeHandler;
     }
 
     @Bean
     public RouterFunction<ServerResponse> mainRouters() {
         return RouterFunctions
                 .route()
+                .GET("/coffees", req -> ServerResponse.ok().contentType(MediaType.APPLICATION_JSON).body(coffeeHandler.allCoffee(req), Coffee.class))
                 .path("/api/v1", builder ->
                         builder.add(cricketAPI.cricketCrickBuzzAPI())
                 )
--- src/main/java/com/cricketcrickbuzz/utils/CustomLogger.java
+++ src/main/java/com/cricketcrickbuzz/utils/CustomLogger.java
@@ -9,11 +9,15 @@ public class CustomLogger {
     private static Logger logger = LoggerFactory.getLogger(CustomLogger.class);
 
     public static void logInRequest(ServerRequest sr){
-        //[org.springframework.web.reactive.function.server.RouterFunctions.uriTemplateVariables, org.springframework.web.reactive.HandlerMapping.bestMatchingHandler,
-        // org.springframework.web.reactive.HandlerMapping.bestMatchingPattern, org.springframework.web.reactive.function.server.RouterFunctions.request,
+        //[org.springframework.web.reactive.function.server.RouterFunctions.uriTemplateVariables,
+        // org.springframework.web.reactive.HandlerMapping.bestMatchingHandler,
+        // org.springframework.web.reactive.HandlerMapping.bestMatchingPattern,
+        // org.springframework.web.reactive.function.server.RouterFunctions.request,
         // org.springframework.web.reactive.HandlerMapping.uriTemplateVariables, org.springframework.web.server.ServerWebExchange.LOG_ID,
         // org.springframework.web.reactive.function.server.RouterFunctions.matchingPattern]
-        logger.info("Id: {}, URL PATH {}, Http headers {}, path variables {}, calling method  {}",sr.attributes().get("org.springframework.web.server.ServerWebExchange.LOG_ID"),sr.path(),sr.headers(), sr.pathVariables(), sr.method() );
+        logger.info("Id: {}, URL PATH {}, Http headers {}, path variables {}, calling method  {}, matchingPattern :{} ",sr.attributes().get("org.springframework.web.server.ServerWebExchange.LOG_ID"),sr.path(),sr.headers(),
+                sr.pathVariables(), sr.method() ,sr.attributes().get("org.springframework.web.reactive.function.server.RouterFunctions.matchingPattern") );
+        logger.info(sr.attributes().toString());
     }
     public static void logOutRequest(ServerRequest serverRequest, ServerResponse sr){
         logger.info("Id: {}, response status code {}, Http headers {}, path variables {}",serverRequest.attributes().get("org.springframework.web.server.ServerWebExchange.LOG_ID"),sr.statusCode(),sr.headers(), sr.toString() );
